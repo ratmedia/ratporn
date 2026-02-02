9f007d78d87141c4068eb75220d9206174b1da2a
ratmedia <whoevermatters@gmail.com>
2026-02-01 19:37:59 -0600

    Delete old update.js

--- update.js
+++ update.js
@@ -1,9 +1,1 @@
-fetch("https://ratmedia.github.io/ratporn/latest.json")
-.then(r => r.json())
-.then(d => {
-  let link = document.createElement("a");
-  link.href = d.latest;
-  link.innerText = "DOWNLOAD LATEST ISSUE";
-  document.body.appendChild(link);
-})
-.catch(() => console.log("Offline mode active"));
+
